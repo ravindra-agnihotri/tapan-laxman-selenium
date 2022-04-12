2f541a16e51e6a3f6e88828a2019989d7d68a394
ravindra.agnihotri01 <ravindra.swt@gmail.com>
2022-04-12 08:43:22 +0530

    committed

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,9 +1,21 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
         import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestClass {
 
     public static void main(String[] args) {
+       // System.setProperty("webdriver.chrome.driver","C:/downloads/chrome");
+        WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+        driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
+
     }
+
+
+
 }
